85701623f986ed85de8ddd510501e3e578ddb161
Evgeny Zaytsev <zaes2000@mail.ru>
2024-10-25 16:06:41 +0100

    fix: add favicon

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -7,6 +7,7 @@ const style = {
     left: '50%',
     transform: 'translate(-50%, -50%)',
     width: '70%',
+    height: '30%',
     bgcolor: 'background.paper',
     border: '2px solid #000',
     boxShadow: 24,
